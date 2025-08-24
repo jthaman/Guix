6d7dc43af9d5407e735a8bc7c80fdbe35e6d116b
John Haman <mail@johnhaman.org>
2025-08-24 15:20:22 -0400

    Trying to get xmonad working

--- config.scm
+++ config.scm
@@ -2,11 +2,14 @@
 ;; used in this configuration.
 (use-modules (gnu)
              (nongnu packages linux)
-             (gnu services xorg)
+             (gnu packages xorg)
+             (gnu system nss)
+             (gnu services desktop)
              (nongnu system linux-initrd)
              (gnu services pm))         ; TLP
 
 (use-service-modules cups desktop networking ssh xorg)
+(use-package-modules suckless wm)
 
 (operating-system
  (kernel linux)
@@ -28,18 +31,26 @@
                %base-user-accounts))
 
  ;; Packages installed system-wide.
- (packages %base-packages)
+ (packages  (append (list
+                     xmonad
+                     dmenu
+                     xterm)
+                    %base-packages))
 
  ;; Services
  (services
   (append
    (list
     (service gnome-desktop-service-type)
+    (set-xorg-configuration
+     (xorg-configuration
+      (keyboard-layout keyboard-layout)))
     (service cups-service-type)
     (service tlp-service-type
              (tlp-configuration
               (cpu-scaling-governor-on-ac (list "performance"))
               (sched-powersave-on-bat? #t))))
+    ;; Common desktop services (dbus, udisks, networking, polkit, etc.)
    %desktop-services))
 
  ;; Bootloader
